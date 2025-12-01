d27c3a1c25f8770f5119eafbf46155f5dd14b9b5
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-12-01 15:32:48 -0300

    Entities Director

---
+++ src/main/java/org/example/entities/Director.java
@@ -0,0 +1,18 @@
+package org.example.entities;
+
+public class Director {
+
+    private String name;
+
+    public Director(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
